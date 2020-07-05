e5a49de076364ffdb7528c7122653ff96f925e0e
nakashima-toshio <nakashima.toshio@icloud.com>
2020-07-05 13:31:15 +0900

    commit-3 add designmemo

--- simpleDjango/settings.py
+++ simpleDjango/settings.py
@@ -71,10 +71,19 @@ WSGI_APPLICATION = 'simpleDjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
+    # 'default': {
+    # 'ENGINE': 'django.db.backends.sqlite3',
+    # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres_db',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'POST': '5432'
     }
 }
 
--- simpleDjango/urls.py
+++ simpleDjango/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.http import HttpResponse
+from django.urls import path, include
+
+
+def home(request):
+    return HttpResponse("Home Page")
+
+
+def about(request):
+    return HttpResponse("About us")
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', home),
+    path('about/', about),
 ]
